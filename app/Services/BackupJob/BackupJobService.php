9daba58b4453d4939e5a505c7e3d68b62342341d
João Bittencourt <j.v_dias@hotmail.com>
2023-09-27 12:43:34 -0300

    wip

--- app/Services/BackupJob/BackupJobService.php
+++ app/Services/BackupJob/BackupJobService.php
@@ -9,6 +9,11 @@ class BackupJobService
 {
     protected PendingRequest $http;
 
+    public function __construct()
+    {
+        $this->http = Http::timeout(30);
+    }
+
     public function get(string $url): BackupJobObject
     {
         return new BackupJobObject(
